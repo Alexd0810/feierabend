0eea1ad37558fa8946c6b2d5c73eee0df8c768e0
Alex <alex.dik2008@gmail.com>
2026-05-11 13:01:13 +0200

    fix: updated url

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -10,9 +10,9 @@ import type { VideoType } from '../types';
  */
 export const videoOptions: Record<VideoType, string> = {
   minecraft: 'n_Dv4JMiwK8',
-  subway:    'jKcVOL_HrPU',
-  satisfying:'3_N6P78tRBM',
-  asmr:      'RQ1fhvMOlvo',
+  subway:    'vTfD20dbxho',
+  satisfying:'UZfHXOJVAJo',
+  asmr:      'vHjHk1CXxbY',
 };
 
 /**
